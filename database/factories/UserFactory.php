0a449ee95d9bba9ec170cfab746bea1484c56890
abrorbeksoft <abrorbek.ubaydullayev98@gmail.com>
2019-12-12 14:14:39 +0500

    bazani shakillantirish tugadi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,11 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => 'Sardor',
+        'surname' => 'Karimov',
+        'lavozim' => $faker->sentence(6),
+        'tel_number' => '+998 71 231-07-53',
+        'level' => $faker->numberBetween(0, 5),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(BolimSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PictureSeeder::class);
+
     }
 }
